7e33dcc6acdf2428cb6983c5586333f6b7c9c64c
Pankaj Shah <aaryanshah615@gmail.com>
2022-12-30 18:12:17 +0545

    new websites

---
+++ News_project/admin/add-category.php
@@ -0,0 +1,29 @@
+<?php include "header.php"; 
+
+  if($_SESSION['role']==0)
+   {
+     header("location:\aryan\News_project\admin\post.php");
+   }                          
+    
+?>
+  <div id="admin-content">
+      <div class="container">
+          <div class="row">
+              <div class="col-md-12">
+                  <h1 class="admin-heading">Add New Category</h1>
+              </div>
+              <div class="col-md-offset-3 col-md-6">
+                  <!-- Form Start -->
+                  <form action="" method="POST" autocomplete="off">
+                      <div class="form-group">
+                          <label>Category Name</label>
+                          <input type="text" name="cat" class="form-control" placeholder="Category Name" required>
+                      </div>
+                      <input type="submit" name="save" class="btn btn-primary" value="Save" required />
+                  </form>
+                  <!-- /Form End -->
+              </div>
+          </div>
+      </div>
+  </div>
+<?php include "footer.php"; ?>
